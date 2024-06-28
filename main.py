4d221f942835d452dfd97d7394fcf6e73a90904f
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-06-28 12:41:11 +0200

    Create simple if-elif construction for RPS scenarios.

--- main.py
+++ main.py
@@ -10,4 +10,44 @@ This is classic rock paper scissors game
 3. Paper wins against rock.
 """
 
+bets = [scissors, rock, paper]
 
+print(welcome_message)
+user_bet = int(input("Choose one: 1 - Rock, 2 - Paper, 3 - Scissors: "))
+computer_bet = random.randint(1, 3)
+
+if user_bet == computer_bet:
+    print("Draw!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
+elif user_bet == 1 and computer_bet == 3:
+    print("You win!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
+elif user_bet == 1 and computer_bet == 2:
+    print("You lost!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
+elif user_bet == 2 and computer_bet == 1:
+    print("You win!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
+elif user_bet == 2 and computer_bet == 3:
+    print("You lost!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
+elif user_bet == 3 and computer_bet == 2:
+    print("You win!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
+elif user_bet == 3 and computer_bet == 1:
+    print("You lost!")
+    print(f"Your bet: {bets[user_bet - 1]}")
+    print("#######################")
+    print(f"Computer bet: {bets[computer_bet - 1]}")
